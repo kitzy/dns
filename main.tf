2d504e20969d2ac63e4fe02728cdc7a7a6f318a2
kitzy <kitzy@kitzy.com>
2025-02-16 13:14:02 -0500

    Create DNS zones if they don't already exist

--- main.tf
+++ main.tf
@@ -13,19 +13,22 @@ locals {
   zones = { for file, content in data.local_file.dns_configs : file => yamldecode(content.content) }
 }
 
-# Fetch Hosted Zones
-data "aws_route53_zone" "zones" {
+# Create Route 53 Hosted Zones if they don't exist
+resource "aws_route53_zone" "zones" {
   for_each = local.zones
   name     = each.value.zone_name
   private_zone = false
+  tags = {
+    "Name" = each.value.zone_name
+  }
 }
 
-# Create DNS Records
+# Create DNS Records in the respective Zone
 resource "aws_route53_record" "records" {
   for_each = merge([for file, zone in local.zones : { for rec in zone.records :
     "${rec.name}.${zone.zone_name}_${rec.type}" => merge(rec, { zone_name = zone.zone_name }) }]...)
 
-  zone_id = data.aws_route53_zone.zones[each.value.zone_name].zone_id
+  zone_id = aws_route53_zone.zones[each.value.zone_name].zone_id
   name    = "${each.value.name}.${each.value.zone_name}"
   type    = each.value.type
   ttl     = each.value.ttl
