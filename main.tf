dca0611f8c26173c8f7ef306658d0aee4a40286f
kitzy <kitzy@kitzy.com>
2025-02-16 17:07:48 -0500

    Dynamic zone creation

--- main.tf
+++ main.tf
@@ -1,36 +1,40 @@
+# Provider setup
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Adjust this to your AWS region
 }
 
-# Get all YAML files from the dns_zones directory
+# Read all YAML files in the dns_zones directory
 locals {
-  zone_files = [for f in fileset("${path.module}/dns_zones", "*.yml") : "${path.module}/dns_zones/${f}"]
+  dns_zones_files = [for file in fileset("${path.module}/dns_zones", "*.yaml") : "${path.module}/dns_zones/${file}"]
+  dns_zones = {
+    for file in local.dns_zones_files : 
+    file => yamldecode(file(file))
+  }
 }
 
-# Decode each YAML file into a usable format
-locals {
-  # Parse the YAML file (which should be in a format that defines multiple zones)
-  dns_zones = yamldecode(file("zones.yaml"))
-}
-
-resource "aws_route53_zone" "zones" {
-  for_each = { for zone in local.dns_zones : zone["zone_name"] => zone }
+# Create Route 53 Zones if they don't exist
+resource "aws_route53_zone" "dns_zones" {
+  for_each = local.dns_zones
 
   name = each.value["zone_name"]
+
+  # This ensures that Route 53 is only created if it does not exist
 }
 
+# Create Route 53 Records for each zone
 resource "aws_route53_record" "dns_records" {
-  for_each = flatten([
-    for zone_key, zone_data in local.dns_zones : [
-      for record in zone_data["records"] : {
-        "${zone_key}_${record["name"]}_${record["type"]}" = record
-      }
-    ]
-  ])
+  for_each = {
+    for zone_file, zone_data in local.dns_zones :
+    for record in zone_data["records"] :
+    "${zone_data["zone_name"]}_${record["name"]}_${record["type"]}" => {
+      zone_name = zone_data["zone_name"]
+      record    = record
+    }
+  }
 
-  zone_id = aws_route53_zone.dns_zones[each.key].zone_id
-  name    = each.value["name"]
-  type    = each.value["type"]
-  ttl     = each.value["ttl"]
-  records = each.value["values"]
+  zone_id = aws_route53_zone.dns_zones[each.value["zone_name"]].id
+  name    = each.value["record"]["name"]
+  type    = each.value["record"]["type"]
+  ttl     = each.value["record"]["ttl"]
+  records = each.value["record"]["values"]
 }
