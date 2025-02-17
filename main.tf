db5566990bf5fd49c7d3c22c827994151ef6a121
kitzy <kitzy@kitzy.com>
2025-02-16 20:25:37 -0500

    Computers were a mistake

--- main.tf
+++ main.tf
@@ -28,28 +28,4 @@ resource "aws_route53_record" "dns_records" {
   lifecycle {
     ignore_changes = [ttl, records]
   }
-
-  count = length([
-    for r in each.value.records :
-    r if !contains([
-      for existing in data.aws_route53_records.existing[each.key].records : existing.name
-    ], r.name)
-  ])
-}
-
-# Fetch the hosted zone ID of each zone from DNS zone files and write to zone_ids.json
-run "Fetching hosted zone IDs" {
-  echo "ZONE_IDS={}" > zone_ids.json  # Initialize an empty JSON object
-  for file in ./dns_zones/*.yml; do
-    zone_name=$(yq e '.zone_name' "$file")
-    zone_id=$(aws route53 list-hosted-zones-by-name --dns-name "$zone_name" --query "HostedZones[0].Id" --output text | sed 's|/hostedzone/||')
-    echo "Fetching hosted zone ID for $zone_name: $zone_id"
-    yq e ". + {\"$zone_name\": \"$zone_id\"}" -i zone_ids.json
-  done
-}
-
-# Fetch existing records from the Route53 hosted zones
-data "aws_route53_records" "existing" {
-  for_each = data.aws_route53_zone.selected
-  zone_id  = each.value.zone_id
 }
